5382a01d2a4a5b87de1fdbdc70199fbc78b66ffb
Anmol Vyas <70644006+TheAverageDetective@users.noreply.github.com>
2025-10-26 00:13:17 +0530

    hmm

--- src/data.h
+++ src/data.h
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// This is a header type file. Data is and function headers are defined here.
+// Actual function bodies are implemented in data.c
+
+// This file will be imported multiple times in the project.
+// To avoid redeclaration this compiler directive is used.
 #ifndef DATA_H
 #define DATA_H
-
 
 typedef struct
 {
     int studentId, bookId;
 
-} Book;
+} Listing;
 
+typedef struct
+{
+    int studentId, bookId
+
+} Request;
 
 #endif
