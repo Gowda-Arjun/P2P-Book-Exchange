10ed3d63ba01c0aa2d795bffbfae8bcd9630958f
Anmol Vyas <70644006+TheAverageDetective@users.noreply.github.com>
2025-10-26 12:05:07 +0530

    something is messed up, commiting to fix it

--- src/data.c
+++ src/data.c
@@ -3,3 +3,43 @@
 #include "data.h"
 
 
+
+Book* createBook (int studentId, int bookId) {
+    Book* book = (Book*) malloc(sizeof(Book));
+    book->studentId = studentId;
+    book->bookId = bookId;
+    book->next = NULL;
+    return book;
+}
+
+
+void insertBook (Book** head, Book* book) {
+
+    if (*head == NULL) {
+        *head = book;
+        return;
+    }
+
+    if ((*head)->bookId > book->bookId) {
+        book->next = *head;
+        *head = book;
+        return;
+    }
+
+    Book* prev = *head, *curr = (*head)->next;
+
+    while (curr != NULL && curr->bookId < book->bookId)
+    {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    prev->next = book;
+    book->next = curr;
+
+}
+
+
+Book* matchExchange() {
+    
+}
